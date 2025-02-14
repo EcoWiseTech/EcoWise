ac03fb8f60950a29d4999655d5ebce90a79c6c88
sgecowisetech <sgecowisetech@gmail.com>
2025-02-14 23:11:43 +0800

    Add conditional if Production then Activate Delete Protection;auto add tags for script and pipeline deployment; update gitignore; update local_deploy_script

--- eco-wise/back-end/local_deploy_script.bat
+++ eco-wise/back-end/local_deploy_script.bat
@@ -5,25 +5,18 @@ echo ==================================================
 echo Starting SAM Build...
 echo ==================================================
 
-call sam build --template "template.yaml" ^
-    --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod"
+call sam build --template "template.yaml" --cached --parallel
 if %ERRORLEVEL% neq 0 (
     echo SAM Build failed. Exiting.
     exit /b 1
 )
 echo SAM Build completed successfully.
 
-echo.
 echo ==================================================
 echo Deploying with SAM (confirm_changeset = true)...
 echo ==================================================
 
-call sam deploy --config-file pipeline/samconfig-pipeline.toml ^
-    --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod" ^
-    --capabilities CAPABILITY_NAMED_IAM ^
-    --config-env "2" ^
-    --role-arn "arn:aws:iam::783764587062:role/aws-sam-cli-managed-prod--CloudFormationExecutionRo-8OFuqwyOFW6X" ^
-    --confirm-changeset
+call sam deploy --stack-name sam-prod --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod" --capabilities CAPABILITY_NAMED_IAM  --role-arn "arn:aws:iam::783764587062:role/aws-sam-cli-managed-prod--CloudFormationExecutionRo-8OFuqwyOFW6X" --confirm-changeset --s3-bucket aws-sam-cli-managed-prod-pipeline--artifactsbucket-1dvm5syvhw4t --s3-prefix sam-prod --tags "Environment=prod"
 if %ERRORLEVEL% neq 0 (
     echo SAM Deploy failed. Exiting.
     exit /b 1
