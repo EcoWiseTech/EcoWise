8160761fc3bc58488fa39be4c89a1c8a488eec77
sgecowisetech <sgecowisetech@gmail.com>
2025-02-14 15:22:57 +0800

    update local_deploy script

--- eco-wise/back-end/local_deploy_script.bat
+++ eco-wise/back-end/local_deploy_script.bat
@@ -3,7 +3,7 @@ echo ==================================================
 echo Starting SAM Build...
 echo ==================================================
 
-sam build --template "template.yaml" --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod"
+call sam build --template "template.yaml" --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod"
 if errorlevel 1 (
     echo SAM Build failed. Exiting.
     exit /b 1
@@ -12,14 +12,15 @@ echo SAM Build completed successfully.
 
 echo.
 echo ==================================================
-echo Starting SAM Deploy...
+echo Creating Change Set with SAM Deploy (--no-execute-changeset)...
 echo ==================================================
 
-sam deploy --config-file pipeline/samconfig-pipeline.toml --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod" --capabilities CAPABILITY_NAMED_IAM --config-env "2" --role-arn "arn:aws:iam::783764587062:role/aws-sam-cli-managed-prod--CloudFormationExecutionRo-8OFuqwyOFW6X"
+call sam deploy --config-file pipeline/samconfig-pipeline.toml --parameter-overrides "ParameterKey=AmplifyStage,ParameterValue=PRODUCTION ParameterKey=Environment,ParameterValue=prod" --capabilities CAPABILITY_NAMED_IAM --config-env "2" --role-arn "arn:aws:iam::783764587062:role/aws-sam-cli-managed-prod--CloudFormationExecutionRo-8OFuqwyOFW6X" --no-execute-changeset
 if errorlevel 1 (
-    echo SAM Deploy failed. Exiting.
+    echo SAM Deploy (change set creation) failed. Exiting.
     exit /b 1
 )
-echo SAM Deploy completed successfully.
-
+echo Change set created successfully.
+echo.
+echo You can now review and execute the change set manually using SAM CLI commands.
 pause
